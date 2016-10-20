6ca53fea48076e900892d8f03cb1daf0da1fc21b
Jan Hruby <jan.hruby@pioneers.io>
2016-10-20 19:04:09 +0200

    update objectfit.vanilla.js functionality

--- app/js/_objectfit.vanilla.js
+++ app/js/_objectfit.vanilla.js
@@ -4,16 +4,18 @@
 
 ;((document) => {
 
-	if('objectFit' in document.documentElement.style === false) {
-		var imgTags = document.getElementsByClassName('image__src');
+	'use strict';
 
-		for (var i = 0; i < imgTags.length; i++) {
-			var imageSource = imgTags[i].querySelector('img').src;
+	if('objectFit' in document.documentElement.style === false) {
+		const imgTags = document.getElementsByClassName('image__src');
+
+		let i = imgTags.length; while (i--) {
+			let imageSource = imgTags[i].querySelector('img').src;
 			imgTags[i].querySelector('img').style.display = 'none';
 			imgTags[i].parent().style.backgroundSize = 'cover';
 			imgTags[i].parent().style.backgroundImage = 'url(' + imageSource + ')';
 			imgTags[i].parent().style.backgroundPosition = 'center center';
 		}
 	}
-	
+
 })(document);
