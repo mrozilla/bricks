340d1b1877c124bbe3c944d101446a1d009d458a
Jan Hruby <jan.hruby@pioneers.io>
2016-11-12 00:26:39 +0100

    add mobile trigger to navbar.vanilla.js

--- app/js/_navbar.vanilla.js
+++ app/js/_navbar.vanilla.js
@@ -10,28 +10,38 @@
 
 		const navbar = {
 			props: Object.assign({
-				target: ".navbar--hiding",
-				class: "is--hidden",
+				target: ".navbar",
+				toggle: ".navbar__toggle",
+				hidingClass: "is--hidden",
+				openClass: "is--open",
+				highlightClass: "burger--highlight",
 				delta: 50
 			}, config),
 			state: {
 				element: null,
+				toggle: null,
 				bottom: null,
 				scrollTop: null,
 				lastScrollTop: 0
 			},
 			init() {
 				if (this.state.element = document.querySelector(this.props.target)) {
-					this.state.bottom = document.body.clientHeight - window.innerHeight;
-					document.addEventListener('scroll', tools.throttle((e) => {
-						this.handleScroll();
-					}, 250));
+					if (this.state.element.classList.contains('navbar--hiding')) {
+						this.state.bottom = document.body.clientHeight - window.innerHeight;
+						document.addEventListener('scroll', tools.throttle((e) => {
+							this.handleScroll();
+						}, 250));
+					}
+					if (this.state.toggle = this.state.element.querySelector(this.props.toggle)) {
+						this.state.toggle.addEventListener('click', (e) => {
+							this.handleClick();
+						})
+					}
 				} else {
 					return;
 				}
 			},
 			handleScroll() {
-				console.log("handledScroll");
 				this.state.scrollTop = window.pageYOffset | document.body.scrollTop;
 				if (Math.abs(this.state.lastScrollTop - this.state.scrollTop) <= this.props.delta) {
 					return;
@@ -47,10 +57,14 @@
 				this.state.lastScrollTop = this.state.scrollTop;
 			},
 			hideNavbar() {
-				this.state.element.classList.add(this.props.class);
+				this.state.element.classList.add(this.props.hidingClass);
 			},
 			showNavbar() {
-				this.state.element.classList.remove(this.props.class);
+				this.state.element.classList.remove(this.props.hidingClass);
+			},
+			handleClick() {
+				this.state.toggle.classList.toggle(this.props.highlightClass);
+				this.state.element.classList.toggle(this.props.openClass);
 			}
 		}
 
